6203e98640a5e4db392cbdc457b1f7285fb177c8
Joshua Cantara <1975723+joshuacant@users.noreply.github.com>
2024-09-19 17:46:55 -0400

    adding a few comments to the previous commits

--- listmenu.lua
+++ listmenu.lua
@@ -540,7 +540,7 @@ function ListMenuItem:update()
                 local bar_and_icons
                 local bar_icon_size = Screen:scaleBySize(23) -- size for icons used with progress bar
                 if status == "complete" and fn_pages > (max_progress_size * pixels_per_page) then
-                    progress_width = progress_width + bar_icon_size
+                    progress_width = progress_width + bar_icon_size -- both icons need 2x half-width added
                     progress_dimen = Geom:new {
                         x = 0, y = 0,
                         w = progress_width,
@@ -551,7 +551,7 @@ function ListMenuItem:update()
                         progress_bar,
                     }
                 elseif status == "complete" then
-                    progress_width = progress_width + math.floor(bar_icon_size/2)
+                    progress_width = progress_width + math.floor(bar_icon_size/2) -- one icon needs 1x half-width added
                     progress_dimen = Geom:new {
                         x = 0, y = 0,
                         w = progress_width,
@@ -562,7 +562,7 @@ function ListMenuItem:update()
                         progress_bar,
                     }
                 elseif fn_pages > (max_progress_size * pixels_per_page) then
-                    progress_width = progress_width + math.floor(bar_icon_size/2)
+                    progress_width = progress_width + math.floor(bar_icon_size/2) -- one icon needs 1x half-width added
                     progress_dimen = Geom:new {
                         x = 0, y = 0,
                         w = progress_width,
@@ -575,7 +575,7 @@ function ListMenuItem:update()
                 else
                     progress_dimen = Geom:new {
                         x = 0, y = 0,
-                        w = progress_width,
+                        w = progress_width, -- no icons needs no width added
                         h = bar_icon_size,
                     }
                     bar_and_icons = CenterContainer:new {
@@ -588,7 +588,7 @@ function ListMenuItem:update()
                 }
                 table.insert(progress_block, bar_and_icons)
 
-                -- books with fn_page_count larger than the max get a plus sign at the left edge of the progress bar
+                -- books with fn_page_count larger than the max get an indicator at the left edge of the progress bar
                 local max_widget = ImageWidget:new({
                     file = getSourceDir() .. "/resources/large_book.svg",
                     width = bar_icon_size,
