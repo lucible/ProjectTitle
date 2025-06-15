5e14042df1de932b08c4867d96a7bb56c1190a83
Joshua Cantara <1975723+joshuacant@users.noreply.github.com>
2025-06-15 12:30:59 -0400

    omit certain files from release build zips

--- build-release-zip.cmd
+++ build-release-zip.cmd
@@ -1,17 +1,22 @@
 @ECHO OFF
 
-#make folder
+REM make folder
 mkdir projecttitle.koplugin
 
-#copy everything into the right folder name
+REM copy everything into the right folder name
 copy *.lua projecttitle.koplugin
 xcopy fonts projecttitle.koplugin\fonts /s /i
 xcopy icons projecttitle.koplugin\icons /s /i
 xcopy resources projecttitle.koplugin\resources /s /i
-copy LICENSE projecttitle.koplugin
 
-#zip the folder
+REM cleanup unwanted
+del /q projecttitle.koplugin\resources\collage.jpg
+del /q projecttitle.koplugin\resources\licenses.txt
+
+REM zip the folder
 7z a -tzip projecttitle.zip projecttitle.koplugin
 
-#delete the folder
+REM delete the folder
 rmdir /s /q projecttitle.koplugin
+
+pause
